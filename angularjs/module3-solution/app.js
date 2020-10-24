bc100c417d8c13e80c5aea3d659d1b7ba8559fd3
Arienne Marshall <ariennemarshall@Arienne-Marshalls-MacBook-Pro-2.local>
2020-10-24 19:00:42 -0400

    3rd Module #3

--- angularjs/module3-solution/app.js
+++ angularjs/module3-solution/app.js
@@ -25,6 +25,7 @@ function NarrowItDownController(MenuSearchService){
 	var menu = this;
 
 	menu.searchTerm = "";
+	//menu.searchTerm = MenuSearchService.searchItem;
 
 	var promise = MenuSearchService.getMatchedMenuItems(menu.searchTerm);
 	promise.then(function (response){
@@ -32,11 +33,11 @@ function NarrowItDownController(MenuSearchService){
 	},
 	function(response){
 		console.log("Error running MenuSearchService.getMatchedMenuItems.")
-	})
+	});
 
 	menu.removeItem = function(itemIndex){
 		MenuSearchService.removeItem(itemIndex);
-	}
+	};
 
 }
 
@@ -45,6 +46,8 @@ MenuSearchService.$inject=['$http'];
 function MenuSearchService($http){
 
 	var service = this;
+
+ 	//var searchTerm = "";
 
 	//List of filtered menu items
 	var foundItems = [];
@@ -57,29 +60,32 @@ function MenuSearchService($http){
 		})
 		.then(function (result) {
 		    // process result and only keep items that match
-		    	var myresult = result.data.menu_items;
-		    	console.log("length", myresult.length, "myresult[1].name",myresult[1].name)
-				for(var i=0; i<myresult.length; i++){
+	    	var myresult = result.data.menu_items;
+	    	console.log("length", myresult.length, "myresult[1].name",myresult[1].name)
+			for(var i=0; i<myresult.length; i++){
 
-					if (myresult[i].name.toLowerCase().indexOf(searchTerm) !== -1){
-						
-						var item = {
-							name: myresult[i].name
-						};
+				if (myresult[i].name.toLowerCase().indexOf(searchTerm) !== -1){
+					
+					console.log('found one that matches ',searchTerm)
+					var item = {
+						name: myresult[i].name
+					};
 
-						foundItems.push(item);	
-						console.log(foundItems);			
-					}
+					foundItems.push(item);	
 				}
-			//}
+			}
+		console.log("first item in found items is ",foundItems[0].name, "and # of items is ", foundItems.length)
+	   // return processed items
+			return foundItems;
 		})
 		.catch(function (errorResponse){
-			console.log("Something went wrong!");
+			console.log("Something went wrong inside getMatchedMenuItems function!");
 
 		});
 
+		//console.log("first item in found items is ",foundItems[0].name, "and has ", foundItems.length)
 	   // return processed items
-	    return foundItems;
+	    //return foundItems;
 	};
 
 
@@ -95,8 +101,8 @@ function MenuSearchService($http){
 
 	service.removeItem = function(){
 		items.splice(itemIndex,1);
-	}
+	};
  
 }
 
-})()
+})();
