8327e2d3962f52f490e795d0ac39a8d478ca97a5
Arienne Marshall <ariennemarshall@Arienne-Marshalls-MacBook-Pro-2.local>
2020-11-01 23:33:45 -0400

    MODULE 4

--- angularjs/module4-solution/src/data.module.js
+++ angularjs/module4-solution/src/data.module.js
@@ -1,7 +1,7 @@
 (function (){
 'use strict';
 
-angular.module('data',['ui.router']);
+angular.module('data',[]);
 
 
 })();
--- angularjs/module4-solution/src/items.controller.js
+++ angularjs/module4-solution/src/items.controller.js
@@ -4,13 +4,50 @@
 angular.module('MenuApp')
 .controller('MenuController', MenuController);
 
-MenuController.$inject = ['myItems'];
-function MenuController(myItems){
+MenuController.$inject = ['$rootScope','myItems'];
+function MenuController($rootScope,myItems){
 
 	var menu = this;
 	menu.items = myItems;
 
 	console.log("the menu items are: ",menu.items);
+
+
+
+	var cancellers = [];
+
+	menu.$onInit = function(){
+
+		var cancel = $rootScope.$on('$stateChangeStart',
+			function(event, toState, toParams, fromState, fromParams, options){
+				console.log('stateChangeStart!');
+			})
+			cancellers.push(cancel);
+
+
+		 cancel = $rootScope.$on('$stateChangeSuccess',
+			function(event, toState, toParams, fromState, fromParams){
+				console.log("stateChangeSuccess!")
+
+			})
+				cancellers.push(cancel);
+
+
+		 cancel = $rootScope.$on('$stateChangeError',
+			function(event, toState, toParams, fromState, fromParams, error){
+				console.log("event: ", event, ", toState: ", toState, ", toParams: ",
+					toParams, ", fromState: ", fromState, ", fromParams: ", fromParams,
+					"error: ", error);
+			});
+
+			cancellers.push(cancel);
+	};
+
+	menu.$onDestroy = function(){
+		cancellers.forEach(function (item){
+			item();
+		}); 
+	};
 }
 
 
