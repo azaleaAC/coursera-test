4875dcf2deb852a9a98ffc6098d976273fac8958
Arienne Marshall <ariennemarshall@Arienne-Marshalls-MacBook-Pro-2.local>
2020-11-07 13:23:04 -0400

    Module 5

--- angularjs/module5-solution/src/public/signup/signup.controller.js
+++ angularjs/module5-solution/src/public/signup/signup.controller.js
@@ -4,7 +4,7 @@
 angular.module('public')
 .controller('SignUpController', SignUpController);
 
-SignUpController.$inject = ['MenuService']
+SignUpController.$inject = ['MenuService'];
 function SignUpController(MenuService) {
   
   var $ctrl = this;
@@ -15,12 +15,11 @@ function SignUpController(MenuService) {
 
 
  $ctrl.getFav = function(MenuService) {
- 	console.log("It's coming into the function!");
   	$ctrl.foundItem = MenuService.getFavorite($ctrl.favItem);
   	console.log($ctrl.foundItem);
   }
 
-  $ctrl.Invalid = function(MenuService) {
+  $ctrl.Invalid = function() {
   	if($ctrl.foundItem == []){
   		$ctrl.success = "Your information has been saved."
   		return true;
