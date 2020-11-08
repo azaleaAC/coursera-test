2e7d3f7b6078a1a4319f43ddf2a1a00fa9055180
Arienne Marshall <ariennemarshall@Arienne-Marshalls-MacBook-Pro-2.local>
2020-11-08 09:27:49 -0400

    Module 5

--- angularjs/module5-solution/src/public/signup/signup.controller.js
+++ angularjs/module5-solution/src/public/signup/signup.controller.js
@@ -18,12 +18,9 @@ function SignUpController(MenuService) {
  	console.log('sending ', userEntry, ' as argument');
   	var myResponse = MenuService.getFavorite(userEntry);
   	console.log('myresponse is ',myResponse);
-  	console.log('response.data is', myResponse.data);
-  	console.log('response.value is ',myResponse.value);
-    console.log('response.d is ', myResponse.d);
-    console.log('response.d.value is ', myResponse.d.value);
-        console.log('response.d.$$state.value is ', myResponse.d.$$state.value);
-        console.log('response.d.$state.value is ', myResponse.d.$state.value);
+  	console.log('response.$state is', myResponse.$state);
+  	console.log('response.$state.value is ',myResponse.$state.value);
+
 	$ctrl.foundItem = myResponse.$state.value;
   	console.log('response is ',$ctrl.foundItem);
   	//console.log('found item is: ',$ctrl.foundItem);
