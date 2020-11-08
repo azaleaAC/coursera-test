22321846bcbbc6f3119b78f20bce5853edac49f5
Arienne Marshall <ariennemarshall@Arienne-Marshalls-MacBook-Pro-2.local>
2020-11-08 15:43:40 -0400

    Module 5

--- angularjs/module5-solution/src/public/signup/signup.controller.js
+++ angularjs/module5-solution/src/public/signup/signup.controller.js
@@ -33,8 +33,8 @@ function SignUpController(MenuService) {
   	var success;
   	var InvalidMsg;
 
-  	
-  	if(ItemFound === {}){
+
+  	if(ItemFound.length === 0){
 
 		InvalidMsg = "Please enter a valid Short Name for the your favorite dish.";
   		return false;
@@ -42,7 +42,7 @@ function SignUpController(MenuService) {
 
   	}
   	else{
-  			success = "Your information has been saved."
+  		success = "Your information has been saved."
   		InvalidMsg = "";
 
   		return true;
